133796340c663d4dde82796667b54313813c257f
David Wei <david.wei4043@gmail.com>
2024-08-26 16:03:38 -0700

    Split Table and TableCell components

---
+++ src/components/AgenticTableCell.tsx
@@ -0,0 +1,78 @@
+import { Flex, Loader, Table, TextInput } from "@mantine/core";
+import { IconFileX } from "@tabler/icons-react";
+import { memo, useState } from "react";
+import { AGENT_FAILED, AGENT_LOADING } from "../constants/AgentTableConstants";
+
+/**
+ * Represents an AgenticTableCellProps interface.
+ */
+type AgenticTableCellProps = {
+  target: string,
+  column: string,
+  value: string | undefined | typeof AGENT_LOADING | typeof AGENT_FAILED,
+  setValue: (value: string) => void
+}
+
+/**
+ * Represents an AgenticTableCell component.
+ * 
+ * This component represents a single cell in the AgenticTable and will be responsible for running the query.
+ * 
+ */
+export const AgenticTableCell = memo(function AgenticTableCell({
+  target,
+  column,
+  value,
+  setValue
+}: AgenticTableCellProps) {
+
+  const [editing, setEditing] = useState<boolean>(false);
+  const [cellValue, setCellValue] = useState<string>(value as string);
+
+  return (
+    <Table.Td key={`${target}-${column}`}>
+      {
+        value == AGENT_FAILED &&
+        <Flex align='center' justify='center'>
+          <IconFileX color='red' />
+        </Flex>
+      }
+      {value == AGENT_LOADING &&
+        <Flex align='center' justify='center'>
+          <Loader size='xs' />
+        </Flex>
+      }
+      {value !== AGENT_FAILED && value !== AGENT_LOADING && !editing &&
+        <Flex align='center' justify='center' onClick={() => setEditing(true)}>
+          {cellValue}
+        </Flex>
+      }
+      {
+        editing &&
+        <Flex align='center' justify='center'>
+          <TextInput
+            value={cellValue}
+            onChange={(e) => {
+              // Update the targetData
+              setCellValue(e.target.value);
+            }}
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                setValue(cellValue);
+                setEditing(false);
+              } else if (e.key === "Escape") {
+                setCellValue(value as string);
+                setEditing(false);
+              }
+            }}
+            onBlur={() => {
+              setValue(value as string);
+              setEditing(false);
+            }}
+          />
+        </Flex>
+      }
+
+    </Table.Td>
+  );
+});
